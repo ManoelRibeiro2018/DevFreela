0eb27c36f0999d932bdd8e30016496d653543141
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-17 20:43:53 -0300

    Criando escopo do projeto

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProjectModel)
+        {
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = 1 },
+                createProjectModel
+                );
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProjectModel)
+        {
+            // Return BadRequeste();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Return BadRequeste();
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("id/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+
+        [HttpPut("id/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+
+}
