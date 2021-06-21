4cc6e147fc064f6abcd7b7a38d346bcfe106b5e2
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-21 14:51:17 -0300

    implemente ProjectService e IProjecteService

---
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -0,0 +1,54 @@
+﻿using DevFreela.Application.InputModel;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class ProjectService : IProjectService
+    {
+        public int Create(ProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateComment(CommentInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjeDetailsViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Application.InputModel;
+using DevFreela.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll();
+        ProjeDetailsViewModel GetById(int id);
+
+        int Create(ProjectInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel);
+        void Delete(int id);
+        void CreateComment(CommentInputModel inputModel);
+        void Start(int id);
+        void Finish(int id);
+
+    }
+}
