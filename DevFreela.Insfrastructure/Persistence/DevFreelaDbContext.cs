d4ca1f0cec6c83599051a9883dcb4971de626100
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-21 11:51:32 -0300

    Criação da camade de infrastruture

---
+++ DevFreela.Insfrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Insfrastructure.Persistence
+{
+    class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>{
+            new Project("Meu projeto 1", "minha drescrição 1", 1, 1, 1000),
+            new Project("Meu projeto 2", "minha drescrição 2", 1, 1, 1000),
+            new Project("Meu projeto 3", "minha drescrição 3", 1, 1, 1000)
+        };
+        }
+       
+        public List<Project> Projects { get; private set; }
+        public List<User> Users { get; private set; }
+        public List<Skill> Skills { get; private set; }
+        public List<ProjectComment> ProjectComment  { get; private set; }
+    }
+}
