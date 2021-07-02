ce16af4206c651976123eb3ce51ab8bbac97cd38
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-07-02 13:26:55 -0300

    Criando método de login

---
+++ DevFreela.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using DevFreela.Application.ViewModel;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand,LoginUserViewModel>
+    {
+        private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
+        public LoginUserCommandHandler(IAuthService authService, IUserRepository userRepository)
+        {
+            _authService = authService;
+            _userRepository = userRepository;
+        }
+
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            //Utiliza o mesmo algoritimo para criar um hash dessa senha
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            //Consulta o usuario pelo email e senha hasheado
+            var user = await _userRepository.GetUserByEmailAndPasswordAsync(request.Email, passwordHash);
+
+            //Tratamento
+            if (user == null)
+            {
+                return null;
+            }
+
+            //se existir, gerar o token com as informações do usuário
+            var token = _authService.GenerateJwtToken(user.Email, user.Role);
+
+            return new LoginUserViewModel(user.Email, token);
+        }
+    }
+}
--- DevFreela.Insfrastructure/Persistence/Repositories/UserRepository.cs
+++ DevFreela.Insfrastructure/Persistence/Repositories/UserRepository.cs
@@ -25,5 +25,9 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
             return await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == id);
         }
 
+        public async Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash)
+        {
+            return await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == passwordHash);
+        }
     }
 }
