8e41c076a3dc83a7d7c0cb487bd7e5d9bb001f85
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-07-05 10:48:51 -0300

    Criando Validator para login

--- DevFreela.Application/Validators/LoginUserValidator.cs
+++ DevFreela.Application/Validators/LoginUserValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevFreela.Application.Commands.LoginUser;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Application.Validators
 {
-    class LoginUserValidator
+    public class LoginUserValidator : AbstractValidator<LoginUserCommand>
     {
+        public LoginUserValidator()
+        {
+            RuleFor(u => u.Email)
+                .EmailAddress()
+                .WithMessage("E-mail inválido");
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                .WithMessage("Senha invalida");
+        }
     }
 }
