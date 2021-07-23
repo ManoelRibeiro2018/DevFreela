58bb7089682eef3f51368d482276cd8475c32de8
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-07-23 12:30:07 -0300

    Criando teste de projetos

---
+++ DevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Core.Entites;
+using DevFreela.Core.Enums;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core
+{
+    public class ProjectTests
+    {
+
+        [Fact]
+        public void TestIfProjectStartWork()
+        {
+            var project = new Project("title project", "Description project", 1, 1, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+
+        }
+    }
+}
