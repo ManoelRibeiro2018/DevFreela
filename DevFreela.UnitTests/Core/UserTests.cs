88aed97f26f29367a0ec87785f74bbd71a2c36cc
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-08-04 21:20:27 -0300

    teste de usuário criado

---
+++ DevFreela.UnitTests/Core/UserTests.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core
+{
+    public class UserTests 
+    {
+        [Fact]
+        public void TestIsExistUser()
+        {
+            var user = new User("fullName", "teste@gmail.com", DateTime.Now, "123456", "admin");
+
+            Assert.NotNull(user.FullName);
+            Assert.NotNull(user.Email);
+            Assert.NotNull(user.Role);
+        }
+    }
+}
